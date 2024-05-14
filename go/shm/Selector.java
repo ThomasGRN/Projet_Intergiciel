085bc55c568c6b92c513e50b6728d4b7823291c8
charlottecolome <chacha.colome@sfr.fr>
2024-05-14 17:27:16 +0200

    select

--- go/shm/Selector.java
+++ go/shm/Selector.java
@@ -1,19 +1,56 @@
 package go.shm;
 
 import go.Direction;
+import go.Observer;
 import go.Channel;
+
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.Semaphore;
 
 public class Selector implements go.Selector {
 
+    private Map<Channel, Direction> channels;
+    private List<Channel> channelReady;
+    private Semaphore semaphore;
+
     public Selector(Map<Channel, Direction> channels) {
-        // TODO
+        this.channels = channels;
+        this.channelReady = new LinkedList();
+        this.semaphore = new Semaphore(0);
+
+        for (Entry<Channel, Direction> entry : channels.entrySet()){
+            entry.getKey().observe(entry.getValue(), new Observer(){
+                @Override
+                public void update(){
+                    channelReady.add(entry.getKey());
+                    semaphore.release();
+                }
+            });
+        }
     }
 
     public Channel select() {
-        // TODO
-        return null;
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Channel retour = channelReady.get(0);
+        Direction directionRetour = channels.get(retour);
+        channelReady.remove(0);
+        retour.observe(Direction.inverse(directionRetour), new Observer(){
+            @Override
+            public void update(){
+                channelReady.add(retour);
+                semaphore.release();
+            }
+        });
+        return retour;
     }
 
 }
