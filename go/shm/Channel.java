c0cbc340702cc82827703d333d2555000b9f1b5e
thomas <thomas.gruner@etu.inp-n7.fr>
2024-05-14 14:24:57 +0200

    feat : v1

--- go/shm/Channel.java
+++ go/shm/Channel.java
@@ -1,26 +1,66 @@
 package go.shm;
 
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
 import go.Direction;
 import go.Observer;
 
 public class Channel<T> implements go.Channel<T> {
 
+    // --- Attributes ---
+    String name;
+    final Semaphore inMutex;
+    final Semaphore outMutex;
+    final Semaphore globMutex;
+    T val;
+
+
+    // --- Methods ---
     public Channel(String name) {
-        // TODO
+        this.name = name;
+        this.inMutex = new Semaphore(0); 
+        this.outMutex = new Semaphore(1);
+        this.globMutex = new Semaphore(1);
+        this.val = null;
     }
     
     public void out(T v) {
-        // TODO
+        try {
+            globMutex.acquire();
+            outMutex.acquire();
+
+            // SC
+            val = v;
+
+
+            inMutex.release();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public T in() {
-        // TODO
+        try {
+            inMutex.acquire();
+            
+            // SC
+            T retour = val;
+            
+            outMutex.release();
+            inMutex.release();
+            globMutex.release();
+            return retour;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     public String getName() {
-        // TODO
-        return null;
+        return name;
     }
 
     public void observe(Direction dir, Observer observer) {
